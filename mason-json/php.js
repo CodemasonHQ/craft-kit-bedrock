a28fb22d7d9988d899b1d64cb88af1318d672e82
benmag <benmag@users.noreply.github.com>
2019-04-07 23:34:27 +1000

    Fix indentation

--- mason-json/php.js
+++ mason-json/php.js
@@ -1,16 +1,16 @@
 module.exports = {
-    name: 'app', 
-    build: '.',
-    base: 'codemasonhq/bedrock', 
-    type: 'service',
-    volumes: {
-        './':'/app',
-    },
-    ports: [
-        "80:80",
-        "443:443"
-    ],
-    environment: {
-        'DOCUMENT_ROOT': '/app/web',
-    }
+  name: 'web', 
+  build: '.',
+  base: 'codemasonhq/bedrock', 
+  type: 'service',
+  volumes: {
+    './':'/app',
+  },
+  ports: [
+    "80:80",
+    "443:443"
+  ],
+  environment: {
+    'DOCUMENT_ROOT': '/app/web',
+  }
 }
